59a4a54bbbf3f1f06621ff3528b698e5c082e543
PranavWalwade <pranavwalwade123@gmail.com>
2025-02-19 19:06:46 +0530

    final commit

---
+++ Backend/p11crud/p11crud/src/main/java/com/knowit/p11crud/P11crudApplication.java
@@ -0,0 +1,15 @@
+package com.knowit.p11crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class P11crudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P11crudApplication.class, args);
+	}
+
+}
